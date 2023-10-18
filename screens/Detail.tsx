4e858645888dd13ad5956e5dd67c810514fa7104
unknown <taemine.song@gmail.com>
2023-10-18 21:24:17 +1100

    updated Details screen

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,6 +1,13 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
-import { Dimensions, StyleSheet, Linking } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  Linking,
+  TouchableOpacity,
+  Share,
+  Platform,
+} from "react-native";
 import styled from "styled-components/native";
 import { Movie, MovieResponse, TV, TVResponse, moviesApi, tvApi } from "../api";
 import Poster from "../components/Poster";
@@ -72,12 +79,42 @@ const Detail: React.FC<DetailScreenProps> = ({
     [isMovie ? "movies" : "tv", params.id],
     isMovie ? moviesApi.detail : tvApi.detail
   );
+  const shareMedia = async () => {
+    const isAndroid = Platform.OS === "android";
+    const homepage = isMovie
+      ? `https://www.imdb.com/title/${data.imdb_id}`
+      : data.homepage;
+    if (isAndroid) {
+      await Share.share({
+        message: `${params.overview}\nCheck it out: ${homepage}`,
+        title: "original_title" in params ? "Movie" : "TV Show",
+      });
+    } else {
+      await Share.share({
+        url: homepage,
+        title: "original_title" in params ? "Movie" : "TV Show",
+      });
+    }
+  };
+  const ShareButton = () => (
+    <TouchableOpacity onPress={shareMedia}>
+      <Ionicons name="share" color="white" size={24} />
+    </TouchableOpacity>
+  );
 
   useEffect(() => {
     setOptions({
       title: "original_title" in params ? "Movie" : "TV Show",
+      headerRight: () => <ShareButton />,
     });
   }, []);
+  useEffect(() => {
+    if (data) {
+      setOptions({
+        headerRight: () => <ShareButton />,
+      });
+    }
+  }, [data]);
   const openYTLink = async (videoID: string) => {
     const baseUrl = `http://m.youtube.com/watch?v=${videoID}`;
     await Linking.openURL(baseUrl);
